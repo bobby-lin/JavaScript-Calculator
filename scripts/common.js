86f906f4607b296b22fa4bca735e73468ccef9e3
Bobby Lin <bobbylinjh@gmail.com>
2015-12-22 14:21:50 +0800

    Added addDisplay() in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -2,3 +2,12 @@
  * Created on: 20/12/15
  *     Author: Bobby Lin
  */
+
+var eqn = "";
+
+function addDisplay(item) {
+    var e = document.getElementById("display");
+    eqn += item;
+    e.value = eqn;
+    console.log(e.value);
+}
